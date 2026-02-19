cef57106fa63ce36c143bd5394071fa84d07a28d
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-18 18:08:45 -0800

    Initial Commit

---
+++ Wish Registry/Wish_RegistryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wish_RegistryApp.swift
+//  Wish Registry
+//
+//  Created by Bryce Campbell on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Wish_RegistryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
