80de334b48932161c5c9d7eb3aa454e9897287b4
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-20 13:55:53 -0800

    make sure computed property does the work.

--- Wish Registry/Models/Store.swift
+++ Wish Registry/Models/Store.swift
@@ -10,6 +10,20 @@ struct Store {
     var items: IdentifiedArrayOf<Item> = []
     
     var sortedItems: IdentifiedArrayOf<Item> {
+        get {
+            IdentifiedArray(uniqueElements: items.sorted { firstItem, secondItem in
+                firstItem.priority > secondItem.priority
+            })
+        }
         
+        set(newValues) {
+            guard let manager = DB.shared.manager else { return }
+            
+            try? manager.updateOrAdd(items: newValues.elements)
+            
+            items = IdentifiedArray(uniqueElements: manager.items)
+        }
     }
+    
+    
 }
