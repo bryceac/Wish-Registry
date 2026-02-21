ec6a3d4d705a0f3e4e3bfa443c24bebff0cc828d
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-21 15:17:57 -0800

    move note registry to view model.

--- Wish Registry/Views/NoteRegistryView.swift
+++ Wish Registry/Views/NoteRegistryView.swift
@@ -7,36 +7,40 @@
 import SwiftUI
 
 struct NoteRegistryView: View {
-    @State var notes: [Note] = []
+    @State var viewModel = ViewModel()
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(notes.indices, id: \.self) { index in
-                    var noteBinding: Binding<Note> {
-                        Binding {
-                            return notes[index]
-                        } set: { newValue in
-                            notes[index] = newValue
-                            try? DB.shared.manager!.update(noteWithID: newValue.id, andContent: newValue.content)
-                        }
-                    }
+                ForEach(viewModel.notes.indices, id: \.self) { index in
                     
-                    NavigationLink(destination: NoteDetailView(note: noteBinding)) {
-                        NoteView(note: notes[index])
+                    NavigationLink(destination: NoteDetailView(note: $viewModel.notes[index])) {
+                        NoteView(note: viewModel.notes[index])
                     }
-                }.onDelete(perform: delete)
+                }.onDelete(perform: viewModel.delete)
             }.onAppear {
-                notes = DB.shared.manager!.notes
+                viewModel.loadNotes()
             }
         }
     }
-    
-    func delete(at offsets: IndexSet) {
-        for index in offsets {
-            let note = notes[index]
-            
-            try? DB.shared.manager?.delete(noteWithID: note.id)
+}
+
+extension NoteRegistryView {
+    @Observable
+    class ViewModel {
+        var notes: [Note] = []
+        
+        func loadNotes() {
+            guard let manager = DB.shared.manager else { return }
+            notes = manager.notes
+        }
+        
+        func delete(at offsets: IndexSet) {
+            for index in offsets {
+                let note = notes[index]
+                
+                try? DB.shared.manager?.delete(noteWithID: note.id)
+            }
         }
     }
 }
