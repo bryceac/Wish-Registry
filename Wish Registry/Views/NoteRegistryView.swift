b07ea0fe16a3b188f5dddf3f1332232a86e1c261
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-22 08:29:56 -0800

    remove view model from notes.

--- Wish Registry/Views/NoteRegistryView.swift
+++ Wish Registry/Views/NoteRegistryView.swift
@@ -7,40 +7,35 @@
 import SwiftUI
 
 struct NoteRegistryView: View {
-    @State var viewModel = ViewModel()
+    @State private var notes: [Note] = []
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(viewModel.notes.indices, id: \.self) { index in
+                ForEach(notes.indices, id: \.self) { index in
                     
-                    NavigationLink(destination: NoteDetailView(note: $viewModel.notes[index])) {
-                        NoteView(note: viewModel.notes[index])
+                    NavigationLink(destination: NoteDetailView(note: $notes[index])) {
+                        NoteView(note: notes[index])
                     }
-                }.onDelete(perform: viewModel.delete)
+                }.onDelete(perform: delete)
             }.onAppear {
-                viewModel.loadNotes()
+                loadNotes()
             }
         }
     }
 }
 
 extension NoteRegistryView {
-    @Observable
-    class ViewModel {
-        var notes: [Note] = []
+    func loadNotes() {
+        guard let manager = DB.shared.manager else { return }
+        notes = manager.notes
+    }
         
-        func loadNotes() {
-            guard let manager = DB.shared.manager else { return }
-            notes = manager.notes
-        }
-        
-        func delete(at offsets: IndexSet) {
-            for index in offsets {
-                let note = notes[index]
+    func delete(at offsets: IndexSet) {
+        for index in offsets {
+            let note = notes[index]
                 
-                try? DB.shared.manager?.delete(noteWithID: note.id)
-            }
+            try? DB.shared.manager?.delete(noteWithID: note.id)
         }
     }
 }
