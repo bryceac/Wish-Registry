92dbb525ce31c8227da077f6abe1d37b3900a8fb
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-22 13:43:10 -0800

    remove unneeded listener stuff.

--- Wish Registry/Models/ItemStore.swift
+++ Wish Registry/Models/ItemStore.swift
@@ -9,35 +9,19 @@ import Foundation
 
 @Observable
 class ItemStore {
-    var items: IdentifiedArrayOf<Item> {
-        didSet {
-            guard let manager = DB.shared.manager else { return }
-            
-            try? manager.updateOrAdd(items: items)
-        }
-    }
+    var items: IdentifiedArrayOf<Item> = []
     
     var sortedItems: IdentifiedArrayOf<Item> {
-        get {
-            IdentifiedArray(uniqueElements: items.sorted { firstItem, secondItem in
+        IdentifiedArray(uniqueElements: items.sorted { firstItem, secondItem in
                 firstItem.priority > secondItem.priority
-            })
-        }
-        
-        set(newValues) {
-            guard let manager = DB.shared.manager else { return }
-            
-            try? manager.updateOrAdd(items: newValues.elements)
-            
-            items = IdentifiedArray(uniqueElements: manager.items)
-        }
-    }
-    
-    init(withItems items: [Item] = []) {
-        self.items = IdentifiedArray(uniqueElements: items)
+        })
     }
     
     func add(item: Item = Item()) {
-        items.append(item)
+        guard let manager = DB.shared.manager else { return }
+        
+        try? manager.add(item: item)
+        
+        items = IdentifiedArray(uniqueElements: manager.items)
     }
 }
